1e7fbeac358d00d3c475c9f781cb5019c35a7eff
kimtechnos <franciskimani559@gmail.com>
2024-07-02 21:22:35 +0300

    updating the navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import "./assets/globals.css";
 import Header from "./componenents/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/componenents/Header/Header.jsx
+++ src/componenents/Header/Header.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./header.css";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div>
-      <h3>hello world</h3>
+    <div className="Navbar">
+      <span className="nav-logo">HomeHaven</span>
+      <div className={`nav-items ${isOpen ? "open" : ""}`}>
+        <Link to="/" onClick={handleLinkClick}>
+          Home
+        </Link>
+        <Link to="/about" onClick={handleLinkClick}>
+          About
+        </Link>
+        <Link to="/services" onClick={handleLinkClick}>
+          Services
+        </Link>
+        <Link to="/contact" onClick={handleLinkClick}>
+          Contact
+        </Link>
+        <Link to="/singup" onClick={handleLinkClick}>
+          sing up
+        </Link>
+        <Link to="/login" onClick={handleLinkClick}>
+          login
+        </Link>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen ? "open" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
     </div>
   );
 };
